d3328b13a5556d42f9f48403a59307f04760ee2c
Lokiwang <wangjian1009@gmail.com>
2018-10-12 10:51:16 +0800

    补充android编译脚本

---
+++ buildtools/android/depends-lwip.mk
@@ -0,0 +1,43 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := lwip
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../cpe/include \
+                    $(LOCAL_PATH)/../../depends/lwip/src/include \
+                    $(LOCAL_PATH)/../../depends/lwip/src/include/ipv4 \
+	            $(LOCAL_PATH)/../../depends/lwip/src/include/ipv6 \
+                    $(LOCAL_PATH)/../custom/lwip \
+
+LOCAL_SRC_FILES := $(addprefix $(LOCAL_PATH)/../../depends/lwip/, \
+                         src/core/timers.c \
+                         src/core/udp.c \
+                         src/core/memp.c \
+                         src/core/init.c \
+                         src/core/pbuf.c \
+                         src/core/tcp.c \
+                         src/core/tcp_out.c \
+                         src/core/sys.c \
+                         src/core/netif.c \
+                         src/core/def.c \
+                         src/core/mem.c \
+                         src/core/tcp_in.c \
+                         src/core/stats.c \
+                         src/core/inet_chksum.c \
+                         src/core/ipv4/icmp.c \
+                         src/core/ipv4/ip4.c \
+                         src/core/ipv4/ip4_addr.c \
+                         src/core/ipv4/ip_frag.c \
+                         src/core/ipv6/ip6.c \
+                         src/core/ipv6/nd6.c \
+                         src/core/ipv6/icmp6.c \
+                         src/core/ipv6/ip6_addr.c \
+                         src/core/ipv6/ip6_frag.c \
+                         src/api/err.c \
+                    ) \
+                    $(addprefix $(LOCAL_PATH)/../custom/lwip/, \
+                         sys.c \
+                         error.c \
+                     )
+
+include $(BUILD_STATIC_LIBRARY)
---
+++ buildtools/android/net-driver-tun.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := net_driver_tun
+LOCAL_EXPORT_CFLAGS += $(if $(filter 0,$(APKD)),,-g)
+LOCAL_CFLAGS += $(if $(filter 0,$(APKD)),,-DDEBUG=1)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../depends/lwip/src/include \
+                    $(LOCAL_PATH)/../../depends/lwip/src/include/ipv4 \
+	            $(LOCAL_PATH)/../../depends/lwip/src/include/ipv6 \
+                    $(LOCAL_PATH)/../custom/lwip \
+                    $(LOCAL_PATH)/../../../cpe/include \
+                    $(LOCAL_PATH)/../../../net/depends/libev/include \
+                    $(LOCAL_PATH)/../../../net/core/include \
+                    $(LOCAL_PATH)/../../driver_tun/include
+LOCAL_LDLIBS := 
+LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../../driver_tun/src/*.c)
+
+include $(BUILD_STATIC_LIBRARY)
