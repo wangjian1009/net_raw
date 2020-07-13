9fcd746b02a8717bd03a717aeb47dc6228d5c59e
Lokiwang <wangjian1009@gmail.com>
2020-07-13 09:25:32 +0800

    增加macos编译能力

--- buildtools/cmake/net-driver-tun.cmake
+++ buildtools/cmake/net-driver-tun.cmake
@@ -4,6 +4,19 @@ file(GLOB net_driver_tun_source ${net_driver_tun_base}/src/*.c)
 
 add_library(net_driver_tun STATIC ${net_driver_tun_source})
 
+if (NET_TUN_USE_DEV_NE)
+  set(net_driver_tun_compile_definitions
+    ${net_driver_tun_compile_definitions}
+    NET_TUN_USE_DEV_NE=1)
+
+  set(net_driver_tun_compile_options
+    ${net_driver_tun_compile_options}
+    "-x" "objective-c")
+endif()
+
+set_property(TARGET net_driver_tun PROPERTY COMPILE_DEFINITIONS ${net_driver_tun_compile_definitions})
+set_property(TARGET net_driver_tun PROPERTY COMPILE_OPTIONS ${net_driver_tun_compile_options})
+
 set_property(TARGET net_driver_tun PROPERTY INCLUDE_DIRECTORIES
   ${CMAKE_CURRENT_LIST_DIR}/../custom/lwip
   ${CMAKE_CURRENT_LIST_DIR}/../../depends/lwip-2.1.1/src/include
